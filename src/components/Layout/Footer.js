498fe5e41972d15c6243009b7676b43e0b93fb81
Gabriel Claudino <gabrielhc@protonmail.com>
2021-11-28 03:24:44 -0300

    feat(footer): create the footer component

---
+++ src/components/Layout/Footer.js
@@ -0,0 +1,15 @@
+const Footer = () => {
+  return (
+    <footer className='relative py-2 bg-gray-200'>
+      <div className='flex flex-wrap items-center justify-center md:justify-between'>
+        <div className='w-full px-4 mx-auto text-center md:w-4/12'>
+          <div className='py-1 text-sm font-semibold text-gray-500'>
+            Copyright © 2021 Gamerhub
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
