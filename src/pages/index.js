4f5c596e549b5e1a4452983aeb4853c867c55acb
Gabriel Claudino <gabrielhc@protonmail.com>
2021-11-28 03:26:09 -0300

    feat(home): create the basic structure of the home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,24 @@
 import Head from 'next/head';
+import Layout from '../components/Layout/Layout';
+import Button from '../components/Button';
 
 export default function Home() {
   return (
-    <div className='flex flex-col min-h-screen'>
+    <Layout>
       <Head>
         <title>Gamerhub</title>
       </Head>
 
-      <main className=''>
-        <h1 className='text-6xl font-bold'>Gamerhub</h1>
-      </main>
-    </div>
+      <h1 className='text-3xl font-bold text-center md:text-5xl mt-7'>
+        Boas vindas, gamer!
+      </h1>
+      <h3 className='mt-12 text-center'>O que deseja fazer agora?</h3>
+
+      <div className='flex flex-col gap-2 p-3 mt-5'>
+        <Button>Visualizar meus grupos</Button>
+        <Button>Buscar novos</Button>
+        <Button>Criar grupo</Button>
+      </div>
+    </Layout>
   );
 }
